eb47de649e9492852b7bd1629926bb93057ee6a0
Jearom Roxas <roxasjearom@gmail.com>
2024-12-07 14:30:19 +0800

    Remove commented dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,50 +50,6 @@ android {
 
 dependencies {
 
-    /*implementation 'androidx.core:core-ktx:1.13.1'
-    implementation 'androidx.appcompat:appcompat:1.7.0'
-    implementation 'com.google.android.material:material:1.12.0'
-    testImplementation 'junit:junit:4.13.2'
-    androidTestImplementation 'androidx.test.ext:junit:1.2.1'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.6.1'
-
-    // Retrofit with Moshi Converter
-    implementation 'com.squareup.retrofit2:converter-moshi:2.9.0'
-
-    // Moshi
-    implementation 'com.squareup.moshi:moshi-kotlin:1.13.0'
-    implementation 'com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3'
-
-    //Room
-    implementation "androidx.room:room-runtime:2.6.1"
-    kapt "androidx.room:room-compiler:2.6.1"
-    implementation "androidx.room:room-ktx:2.6.1"
-
-    // Coroutine Lifecycle Scopes
-    implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6"
-    implementation "androidx.lifecycle:lifecycle-runtime-ktx:2.8.6"
-
-    //Hilt
-    implementation "com.google.dagger:hilt-android:2.48"
-    kapt "com.google.dagger:hilt-android-compiler:2.48"
-
-    // Coil
-    implementation "io.coil-kt:coil:2.2.2"
-    implementation"io.coil-kt:coil-compose:2.2.2"
-
-    //Tests
-    testImplementation "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4"
-    testImplementation "com.google.truth:truth:1.1.3"
-
-    // Compose
-    implementation 'androidx.activity:activity-compose:1.9.3'
-    implementation 'androidx.compose.material:material:1.7.4'
-    implementation 'androidx.compose.animation:animation:1.7.4'
-    implementation 'androidx.compose.ui:ui-tooling:1.7.4'
-    implementation 'androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6'
-    implementation "androidx.compose.material3:material3:1.3.0"
-    implementation "androidx.constraintlayout:constraintlayout-compose:1.1.0-rc01"*/
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
